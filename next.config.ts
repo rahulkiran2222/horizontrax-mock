bf3e9449c86cd62ba8e40787bf7eeee9423f132b
rk2222 <rahulkiran2222@gmail.com>
2026-01-26 23:42:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This is what Cloudflare is looking for
-  images: {
-    unoptimized: true, // Required for static export
+  output: "standalone",
+  experimental: {
+    serverActions: true,
   },
 };
 
