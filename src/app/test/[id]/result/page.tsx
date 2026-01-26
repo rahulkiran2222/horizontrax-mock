79f8940dfc437f297dcbf8409f97867936934ff2
rk2222 <rahulkiran2222@gmail.com>
2026-01-26 22:55:57 +0530

    Update page.tsx

--- src/app/test/[id]/result/page.tsx
+++ src/app/test/[id]/result/page.tsx
@@ -1,11 +1,14 @@
 import { examData } from "@/data/questions";
 import ResultClient from "./ResultClient";
 
+// This function MUST be in a file without "use client"
 export async function generateStaticParams() {
-  return Object.keys(examData).map((id) => ({ id }));
+  return Object.keys(examData).map((id) => ({
+    id: id,
+  }));
 }
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const resolvedParams = await params;
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await props.params;
   return <ResultClient id={resolvedParams.id} />;
 }
