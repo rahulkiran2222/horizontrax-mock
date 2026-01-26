380ee69d846e27c76c9d1d2de49ccd4acc43eb32
rk2222 <rahulkiran2222@gmail.com>
2026-01-26 20:21:47 +0530

    Update page.tsx

--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,10 +1,14 @@
 import TestClient from "./testclient";
 
 export function generateStaticParams() {
-  return [{ id: 'cpp-oops' }, { id: 'java-oops' }, { id: 'aptitude' }];
+  return [
+    { id: 'cpp-oops' },
+    { id: 'java-oops' },
+    { id: 'aptitude' }
+  ];
 }
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const resolvedParams = await params;
-  return <TestClient params={Promise.resolve(resolvedParams)} />;
+  return <TestClient id={resolvedParams.id} />;
 }
