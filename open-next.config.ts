4dae8adc3bd9ab0c51005de619a98ce449c7314d
rk2222 <rahulkiran2222@gmail.com>
2026-01-27 12:51:51 +0530

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,7 +2,21 @@ const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
+      converter: "edge",
     },
+  },
+
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+
+  dangerous: {
+    enableCacheInterception: false,
   },
 };
 
