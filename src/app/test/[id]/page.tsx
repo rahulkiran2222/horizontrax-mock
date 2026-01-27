11bcbe0ac74b47d202c95f6f29f44d001608909a
rk2222 <rahulkiran2222@gmail.com>
2026-01-27 21:09:01 +0530

    Update page.tsx

--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -5,7 +5,7 @@ export async function generateStaticParams() {
   return Object.keys(examData).map((id) => ({ id }));
 }
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const resolvedParams = await props.params;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params;
   return <TestClient id={resolvedParams.id} />;
 }
